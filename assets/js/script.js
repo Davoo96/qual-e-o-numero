812cc3d9c1f8ae4be4db26747a029cf7abfb2228
David Reyes <davizi96@hotmail.com>
2021-12-22 00:11:51 -0300

    Ajustando o request

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,38 @@
-// fazendo um http request para o randomNumber
-const theUrl = 'https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300';
-function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
-}
-
-console.log(httpGet(theUrl));
-
-let answer = Math.floor(Math.random()*300) + 1;
-
 // declarando variaveis e inserindo elementos do HTML dentro delas
 let guess = document.getElementById('guess');
 const msg1 = document.getElementById('mensagem1');
 const sendBtn = document.getElementById('send-btn');
 let guessedNumber = document.getElementById('guessedNumber');
+let answer;
+
+
+
+function randomNumber() {
+  // fazendo um http request para o número aleatório com Ajax
+  const Http = new XMLHttpRequest(); // inicializando o request
+  const url = 'https://us-central1-ss-devops.cloudfunctions.net/rand?min=1&max=300'; // Colocando o url em uma variavel que vai ser utilizado para definir o endpoint
+  Http.open("GET", url); // junto o "GET" com o endpoint do url
+  Http.send(); // enviando o request para poder receber o objeto 
+
+  // Verificando o estado do request
+  Http.onreadystatechange=function() {
+    if(this.readyState==4) { // se o state for == 4 significa que o request foi feito
+      // se o request foi feito pego o objeto e separo so o valor que preciso do JSON
+      let myJSON = Http.responseText;
+      let myObj = JSON.parse(myJSON);
+      answer = myObj.value; // pegando o valor que esta dentro de "value"
+      console.log(answer); // verificar no console que o valor certo foi pego
+    } else if ( this.status!=200) {
+      msg1.textContent = 'Erro';
+      guessedNumber.value = 502;
+    }
+  }
+}
+
+console.log(randomNumber());
 
 // Função para definir se a resposta for maior, menor ou se acertou o número.
-function send() {
+function sendNumber() {
   let user_guess = Number(guess.value);
   // Conferindo que o usuário digite um número entre 1 e 300
   if(user_guess < 1 || user_guess > 300) {
@@ -26,21 +40,26 @@ function send() {
   } else {
     if(user_guess < answer) {
       msg1.textContent = 'É maior';
+      // mostrando o número do usuário no LED e deixando vazia a caixa de respostas
+      guessedNumber.innerHTML = user_guess;
+      guess.value = '';
+      guess.focus();
     } else if(user_guess > answer) {
       msg1.textContent = 'É menor';
+      guessedNumber.innerHTML = user_guess;
+      guess.value = '';
+      guess.focus();
     } else if(user_guess == answer) {
       msg1.textContent = 'Você acertou!!!!';
+      guessedNumber.innerHTML = user_guess;
+      guess.value = '';
+      guess.focus();
       setGameOver();
     }
   }
-
-  // mostrando o número do usuário no LED e deixando vazia a caixa de respostas
-  guessedNumber.innerHTML = user_guess;
-  guess.value = '';
-  guess.focus();
 }
 
-// Criando função para finalizar o jogo e criando o botão para jogar novamente
+// Função para finalizar o jogo e criando o botão para jogar novamente
 function setGameOver() {
   guess.disabled = true;
   sendBtn.disabled = true;
@@ -52,12 +71,12 @@ function setGameOver() {
   playAgain.addEventListener('click', newGame);
 }
 
+// Função para reiniciar um novo jogo
 function newGame() {
   playAgain.parentNode.removeChild(playAgain);
   guess.disabled = false;
   sendBtn.disabled = false;
   guessedNumber.innerHTML = 0;
   msg1.textContent = '';
+  answer = randomNumber();
 }
-
-console.log(answer);
